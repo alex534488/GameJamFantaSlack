8b55049dd1557d00505ed8efa7498a2a5e5bac7b
William Déry <dery.william@hotmail.com>
2019-08-03 20:26:50 -0400

    Added functions

--- GMTGJ_2019_MisterFantaSlack/Assets/personal_folders/Will/Scripts/Buttons_onClick.cs
+++ GMTGJ_2019_MisterFantaSlack/Assets/personal_folders/Will/Scripts/Buttons_onClick.cs
@@ -8,35 +8,41 @@ public class Buttons_onClick : MonoBehaviour
     public void Restart()
     {
         Debug.Log("Restart!");
+        InputManager.Instance.StartReset();
     }
 
     public void GoToMainMenu()
     {
         Debug.Log("GoToMainMenu!");
+        InputManager.Instance.StartPause();
     }
 
     public void OrderFire()
     {
-        Debug.Log("OrderFire!");
+        InputManager.Instance.StartFire();
     }
 
     public void OrderUp()
     {
         Debug.Log("OrderUp!");
+        InputManager.Instance.StartMove(EDirection.Up);
     }
 
     public void OrderDown()
     {
         Debug.Log("OrderDown!");
+        InputManager.Instance.StartMove(EDirection.Down);
     }
 
     public void OrderLeft()
     {
         Debug.Log("OrderLeft!");
+        InputManager.Instance.StartMove(EDirection.Left);
     }
 
     public void OrderRight()
     {
         Debug.Log("OrderRight!");
+        InputManager.Instance.StartMove(EDirection.Right);
     }
 }
