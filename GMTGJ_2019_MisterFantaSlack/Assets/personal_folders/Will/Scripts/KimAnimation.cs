c0fad3cfc60dddf3f70f17d81d457ec0e70facc8
William Déry <dery.william@hotmail.com>
2019-08-04 11:06:03 -0400

    Added hud button state and basic kim angry animation

---
+++ GMTGJ_2019_MisterFantaSlack/Assets/personal_folders/Will/Scripts/KimAnimation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KimAnimation : MonoBehaviour
+{
+    public static KimAnimation Instance = null;
+    void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+    }
+
+    void Start()
+    {
+        this.DelayedCall(3, () =>
+        {
+            Instance.AnimationAngryKim();
+        });  
+    }
+
+    public void AnimationAngryKim()
+    {
+        GetComponent<Animator>().Play("Angery");
+    }
+}
