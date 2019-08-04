bf998e236ec3ee8c7f80bb6cc975780a4d15b7ff
Xeal <alexandre-blanchet94@hotmail.com>
2019-08-03 23:52:13 -0400

    adding new script for me and deleting useless thing

---
+++ GMTGJ_2019_MisterFantaSlack/Assets/personal_folders/Alex/Script/AlexTestScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlexTestScript : MonoBehaviour
+{
+    public AudioPlayable sound;
+    public AudioAssetGroup groupSound;
+    public AudioSource source;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            sound.PlayOn(source);
+            groupSound.PlayOn(source);
+        }
+    }
+}
