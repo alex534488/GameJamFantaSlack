4e0464692de6baac04d642bcef52bdc01338f2ac
William Déry <dery.william@hotmail.com>
2019-08-03 20:13:22 -0400

    Empty buttons functions

---
+++ GMTGJ_2019_MisterFantaSlack/Assets/personal_folders/Will/Scripts/Buttons_onClick.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Buttons_onClick : MonoBehaviour
+{
+   
+    public void Restart()
+    {
+        Debug.Log("Restart!");
+    }
+
+    public void GoToMainMenu()
+    {
+        Debug.Log("GoToMainMenu!");
+    }
+
+    public void OrderFire()
+    {
+        Debug.Log("OrderFire!");
+    }
+
+    public void OrderUp()
+    {
+        Debug.Log("OrderUp!");
+    }
+
+    public void OrderDown()
+    {
+        Debug.Log("OrderDown!");
+    }
+
+    public void OrderLeft()
+    {
+        Debug.Log("OrderLeft!");
+    }
+
+    public void OrderRight()
+    {
+        Debug.Log("OrderRight!");
+    }
+}
