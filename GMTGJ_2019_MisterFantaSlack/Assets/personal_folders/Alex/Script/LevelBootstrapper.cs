e257841836642b0cc19c69ee9bb2fa3be00b9675
Xeal <alexandre-blanchet94@hotmail.com>
2019-08-04 13:24:17 -0400

    level selector

--- GMTGJ_2019_MisterFantaSlack/Assets/personal_folders/Alex/Script/LevelBootstrapper.cs
+++ GMTGJ_2019_MisterFantaSlack/Assets/personal_folders/Alex/Script/LevelBootstrapper.cs
@@ -8,15 +8,20 @@ public class LevelBootstrapper : MonoBehaviour
     public SceneLinks sceneLinks;
     public LevelsList levelList;
 
+    public static bool startingFromMainMenu = false;
+
     private static bool hasStarted = false;
 
     void Awake()
     {
-        if (!hasStarted)
+        if (!startingFromMainMenu)
         {
-            hasStarted = true;
-            PlayerPrefs.SetInt(SaveKeys.MAX_LEVEL_REACHED, levelList.GetIndexOfLevel(SceneManager.GetActiveScene().name));
-            SceneManager.LoadScene(sceneLinks.PersistantGameScene, LoadSceneMode.Single);
+            if (!hasStarted)
+            {
+                hasStarted = true;
+                PlayerPrefs.SetInt(SaveKeys.MAX_LEVEL_REACHED, levelList.GetIndexOfLevel(SceneManager.GetActiveScene().name));
+                SceneManager.LoadScene(sceneLinks.PersistantGameScene, LoadSceneMode.Single);
+            }
         }
     }
 }
---
+++ GMTGJ_2019_MisterFantaSlack/Assets/personal_folders/Alex/Script/SelectLevelDropDown.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class SelectLevelDropDown : MonoBehaviour
+{
+    public TMP_Dropdown dropdown;
+    public LevelsList levelList;
+
+    void Start()
+    {
+        List<string> options = new List<string>();
+
+        options.Add("Choose Level");
+
+        foreach (string level in levelList.levelSceneName)
+        {
+            options.Add(level);
+        }
+
+        dropdown.ClearOptions();
+
+        dropdown.AddOptions(options);
+    }
+}
